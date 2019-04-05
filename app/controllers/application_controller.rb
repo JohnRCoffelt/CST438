999580983fdceae3639844d265bb64aae2b63158
EC2 Default User <ec2-user@ip-172-31-36-0.us-east-2.compute.internal>
2019-04-05 22:44:18 +0000

    session login working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :authorize
+    protected
+    def authorize
+        unless User.find_by(id: session[:user_id])
+            redirect_to login_url, notice:"Perhaps you still need to sign up?"
+        end
+    end
 end
